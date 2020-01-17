b44725dbfce86175eeb04eededad35f66e5bc03d
Daniel <fvmiyeh@icloud.com>
2020-01-17 19:09:27 +0000

    StackArray struct.

--- libadt/main.c
+++ libadt/main.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "list.h"
-#include "SingleLinkedList/singleLinkedList.h"
+
+#include "Stack/stackArray.h"
 
 int main()
 {
-    struct SingleList* sll = singleListConst();
-    struct DoubleList* list = doubleListConst();
-    singleListAdd(sll, 1);
-    singleListAddFront(sll, 2);
-    singleListAdd(sll, 9);
+    struct StackArray* stack = stackArrayConst(25);
 
-    printf("%d\n", singleListSearch(sll, 2));
+    for(int i=0; i<10; i++)
+    {
+        stackArrayPush(stack, i);
+    }
+    stackArrayPrint(stack);
+    stackArrayPop(stack);
+    stackArrayPrint(stack);
 
-  //  singleListPrint(sll);
-
-    singleListDest(sll);
+    stackArrayDest(stack);
+    return 0;
 }
